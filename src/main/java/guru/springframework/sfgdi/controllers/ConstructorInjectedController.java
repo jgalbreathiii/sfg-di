0aed2a5f7e20de865361e07599f6dc5b89841af0
jgalbreathiii <jgalbreathiii@gmail.com>
2020-05-15 15:39:33 -0400

    Added content from Lecture 35

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,11 +1,14 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController
 {
     private final GreetingService greetingService;
 
+    // Autowired annotation not required for Constructor-based Dependency Injection In Spring 5
     public ConstructorInjectedController(GreetingService greetingService)
     {
         this.greetingService = greetingService;
