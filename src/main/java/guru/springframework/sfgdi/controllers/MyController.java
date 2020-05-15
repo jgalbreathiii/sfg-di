83c13b3b729301242dbdf5892abf0d0dd1a3c1e4
jgalbreathiii <jgalbreathiii@gmail.com>
2020-05-15 16:09:40 -0400

    Added content from Lecture 37

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,15 +1,22 @@
 package guru.springframework.sfgdi.controllers;
 
+import guru.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyController
 {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService)
+    {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello()
     {
-        System.out.println("Hello World!!!");
-
-        return "Hi Folks!";
+        return greetingService.sayGreeting();
     }
 
 }
