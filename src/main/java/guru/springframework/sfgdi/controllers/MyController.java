4d7db753ef07911c20c6a9c415274ea012dbf38a
jgalbreathiii <jgalbreathiii@gmail.com>
2020-05-15 15:21:22 -0400

    Added content from Lecture 34

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -7,7 +7,7 @@ public class MyController
 {
     public String sayHello()
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!");
 
         return "Hi Folks!";
     }
