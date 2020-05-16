be4b9d1f4be126a2ac11b77b901fe7926b9c08d4
jgalbreathiii <jgalbreathiii@gmail.com>
2020-05-15 23:06:52 -0400

    Added content from Lecture 41 and Assignment 4

---
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 12/28/19.
+ */
+
+@Profile({"CAT", "default"})
+@Service("petService")
+public class CatPetService implements PetService
+{
+    @Override
+    public String getPetType()
+    {
+        return "Cats Are the Best!";
+    }
+}
---
+++ src/main/java/guru/springframework/sfgdi/services/PetService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgdi.services;
+
+/**
+ * Created by jt on 12/28/19.
+ */
+public interface PetService
+{
+
+    String getPetType();
+}
